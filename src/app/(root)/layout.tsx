5dc4d16e0e06652a5f97047f96ad78734c701f32
TeeGreat <toyinatofarati64@gmail.com>
2024-04-27 11:04:29 +0100

    clerk auth working

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from '@/components/shared/Sidebar'
+import React from 'react'
+
+const Layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <main className='root'>
+      {/* sidebar component */}
+      <Sidebar />
+      
+      {/* mobile nav */}
+
+        <div className="root-container">
+            <div className="wrapper">
+              {children}
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Layout
